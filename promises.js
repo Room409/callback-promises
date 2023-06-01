38bc630016c378856b99e35c49f96081918a8c1f
Martin H <martinhj.sama@gmail.com>
2023-05-31 21:28:34 -0500

    Caminos

--- promises.js
+++ promises.js
@@ -1,16 +1,16 @@
 const catNames = new Promise(function(resolve, reject){
     const catNames = ['kan','kibo','kiba','kira']
-    resolve(catNames)
-    reject('Error')
+    resolve(catNames) //Camino que se ejecutara si se llega a cumplir la promesa
+    reject('Error')  //Camino que se ejecutara si no se llega a cumplir la promesa
 });
 
 catNames
     .then(function(names){
-        console.log('Nombres de gatos', names)
-    })
+        console.log('Nombres de gatos', names) 
+    }) //Si la promesa se cumple .then se ejecutara
     .catch(function(error){
         console.log('error', error)
-    })
+    }) //Si la promesa no se ejecuta .catch se ejecutara
     .finally(function(){
         console.log('Terminado')
-    })
+    }) //Finally se ejecuta cuando la promesa se ejecuto y se finalizo
